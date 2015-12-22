0d16e16a2186ed1af05cfbead4862d4974cd90a6
Kate Young <=>
2015-12-22 12:34:59 -0800

    add uniqueness validation to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :question_set
-  validates :value, presence: true
+  validates :value, presence: true, uniqueness: { scope: :question_set_id }
   after_initialize :used => false
 end
