87e069376e9b6d7f64d10e787c19268185889242
Kate Young <=>
2015-12-23 15:43:42 -0800

    Add specs for editting questions

--- spec/features/question_sets/edit_spec.rb
+++ spec/features/question_sets/edit_spec.rb
@@ -2,9 +2,33 @@ require 'rails_helper'
 
 describe "Editting a question set" do
    def edit_question_set(options={})
+     options[:name] ||= "New Name"
+     options[:description] ||= "This is a new description."
+
+     visit '/question_sets'
+     within "#question_set_#{question_set.id}" do
+       click_link "Edit"
+     end 
+
+     fill_in "Name", with: options[:name]
+     fill_in "Description", with: options[:description]
+     click_button "Edit Question Set" 
+     options
    end
-   
-   before(:all) do
-      QuestionSet.create(name: "Table Topics Questions", description: "List of questions for table topics")
+
+   let!(:question_set) { FactoryGirl.create(:question_set) }
+
+   it "redirects to question set list on success" do
+     editted_question_set = edit_question_set question_set: question_set
+
+     expect(page).to have_content editted_question_set[:name]
+     expect(page).to have_content "Question set was successfully updated"
+   end
+
+   it "displays an error when title is blank" do
+     edited_question_set = edit_question_set question_set: question_set, name: ""
+
+     expect(page).to have_content edited_question_set[:description]
+     expect(page).to have_content "can't be blank"
    end
 end
