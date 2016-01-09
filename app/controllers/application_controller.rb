d7857743f1844cd209c26458e8b81ecaea1cb09f
kate-young <S1391628@student.mcckc.edu>
2016-01-09 11:07:09 -0600

    Redirect to login page if the user is not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_filter :require_login
+  skip_before_filter :require_login
   protect_from_forgery with: :exception
   add_flash_types :success
 
   def current_user
     User.find(session[:user_id])
+  end
+
+  def require_login
+    unless session[:user_id] 
+      redirect_to login_path
+    end
   end
 
 end
