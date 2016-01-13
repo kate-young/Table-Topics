b45d8e2e9b8c00a242a716417e74f99dbced5804
kate-young <S1391628@student.mcckc.edu>
2016-01-12 19:48:34 -0600

    Test that it redirects to the login page when a user is not logged in

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -14,50 +14,52 @@ RSpec.describe QuestionsController, type: :controller do
   let(:user) { FactoryGirl.create(:user) }
   let(:question_set) { user.question_sets.create({name: "Name", description: "Description"}) }
  
-  before { sign_in(user) }
+  context "Signed in" do
+    before { sign_in(user) }
 
-  describe "GET new" do
-    it "assigns a new question as @question" do
-      get :new, { :question_set_id => question_set.to_param }, valid_session
-      expect(assigns(:question)).to be_a(Question)
-    end
-  end
-
-  describe "POST create" do
-    context "with valid params" do
-      it "creates a new question" do
-        expect {
-          post :create, { :question_set_id => question_set.to_param, question: valid_attributes }, valid_session
-        }.to change(question_set.questions, :count).by(1)
+    describe "GET new" do
+      it "assigns a new question as @question" do
+        get :new, { :question_set_id => question_set.to_param }, valid_session
+        expect(assigns(:question)).to be_a(Question)
       end
     end
-    context "with invalid params" do
-      it "doesn't create a new question" do
-        expect {
+
+    describe "POST create" do
+      context "with valid params" do
+        it "creates a new question" do
+          expect {
+            post :create, { :question_set_id => question_set.to_param, question: valid_attributes }, valid_session
+          }.to change(question_set.questions, :count).by(1)
+        end
+      end
+      context "with invalid params" do
+        it "doesn't create a new question" do
+          expect {
+            post :create, { :question_set_id => question_set.to_param, question: invalid_attributes }, valid_session
+          }.to change(question_set.questions, :count).by(0)
+        end
+        it "redirects to new question" do
           post :create, { :question_set_id => question_set.to_param, question: invalid_attributes }, valid_session
-        }.to change(question_set.questions, :count).by(0)
-      end
-      it "redirects to new question" do
-        post :create, { :question_set_id => question_set.to_param, question: invalid_attributes }, valid_session
-        expect(response).to render_template(:new)
+          expect(response).to render_template(:new)
+        end
       end
     end
-  end
 
-  describe "GET #edit" do
-    let!(:question) { question_set.questions.create(valid_attributes) }
-    it "assigns the requested question as @question" do
-      get :edit, { :question_set_id => question_set.to_param, id: question.to_param}, valid_session
-      expect(assigns(:question)).to eq(question)
+    describe "GET #edit" do
+      let!(:question) { question_set.questions.create(valid_attributes) }
+      it "assigns the requested question as @question" do
+        get :edit, { :question_set_id => question_set.to_param, id: question.to_param}, valid_session
+        expect(assigns(:question)).to eq(question)
+      end
     end
-  end
 
-  describe "DELETE #destroy" do
-    let!(:question) { question_set.questions.create(valid_attributes) }
-    it "deletes the question" do
-      expect {
-         delete :destroy, {:question_set_id => question_set.to_param, :id => question.to_param}, valid_session
-      }.to change(question_set.questions, :count).by(-1)
+    describe "DELETE #destroy" do
+      let!(:question) { question_set.questions.create(valid_attributes) }
+      it "deletes the question" do
+        expect {
+          delete :destroy, {:question_set_id => question_set.to_param, :id => question.to_param}, valid_session
+        }.to change(question_set.questions, :count).by(-1)
+      end
     end
   end
 end
